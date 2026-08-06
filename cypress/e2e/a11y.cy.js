057dccee564fa9736c10067beadc5693d34dadb7
Walmyr <wlsf82@gmail.com>
2026-08-07 01:21:14 +0200

    Test image-alt issue (pass and fail scenarios)

--- cypress/e2e/a11y.cy.js
+++ cypress/e2e/a11y.cy.js
@@ -24,4 +24,20 @@ describe('Simulado Bank', () => {
       runOnly: ["color-contrast"],
     });
   })
+
+  // The below test should fail as the app under test has image-alt issues
+  it('finds no "image-alt" a11y issue', () => {
+    cy.checkA11y(null, {
+      runOnly: ["image-alt"],
+    });
+  })
+
+  // The below test should pass as we're toggling the a11y mode before checking a11y
+  it('finds no "image-alt" a11y issue', () => {
+    cy.get('.switch').click()
+
+    cy.checkA11y(null, {
+      runOnly: ["image-alt"],
+    });
+  })
 })
