f89d8e6ad8ee06c3c9150f413c2e5225a2ce0e9f
Walmyr <wlsf82@gmail.com>
2026-08-07 01:22:43 +0200

    Test button-name issue (pass and fail scenarios)

--- cypress/e2e/a11y.cy.js
+++ cypress/e2e/a11y.cy.js
@@ -72,4 +72,20 @@ describe('Simulado Bank', () => {
       runOnly: ["link-name"],
     });
   })
+
+  // The below test should fail as the app under test has button-name issues
+  it('finds no "button-name" a11y issue', () => {
+    cy.checkA11y(null, {
+      runOnly: ["button-name"],
+    });
+  })
+
+  // The below test should pass as we're toggling the a11y mode before checking a11y
+  it('finds no "button-name" a11y issue', () => {
+    cy.get('.switch').click()
+
+    cy.checkA11y(null, {
+      runOnly: ["button-name"],
+    });
+  })
 })
