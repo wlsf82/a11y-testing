842464f1e51f0679693b9c379279a7872b05faa1
Walmyr <wlsf82@gmail.com>
2026-08-07 01:05:44 +0200

    Configure cypress baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl: 'http://localhost:3000',
     fixturesFolder: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
